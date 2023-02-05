30de38627cf197613749cb29105ce8f3f9908936
Maxim Tam <mtamdev@gmail.com>
2023-02-05 14:21:38 -0500

    Implement addCopyPermalinkButtonToSidebar
    On 2023-01-31, StoriesOnBoard updated the release view of a single
    swimlane to include many useful features but removed a button to copy
    a permament link of a card for the story map view. This extension adds
    back this "Copy permalink" button to the sidebar when a user is viewing
    a card inside a release view.

---
+++ readme.md
@@ -0,0 +1,12 @@
+# StoriesOnBoard Extensions
+
+## How to release
+### Firefox
+
+```bash
+cd src/
+zip -r -FS ../storiesonboard_extensions.zip * --exclude '*.git*'
+```
+
+## References
+https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_first_WebExtension
---
+++ src/storiesonboard_extensions.js
@@ -0,0 +1,61 @@
+function addCopyPermalinkButtonToSidebar() {
+  var personaLink = document.querySelector('[id^="card-persona-menu-"]');
+  if (personaLink && !document.querySelector("#sobe__copy-permalink-button")) {
+    var sidebarContainer = document.querySelector(".card-body-right");
+    var roadmapCardId = personaLink.id.replace("card-persona-menu-", "");
+    var button = `
+      <br>
+      <button
+        type="button"
+        class="sobe__button"
+        id="sobe__copy-permalink-button"
+      >
+        Copy permalink
+      </button>
+    `;
+    sidebarContainer.innerHTML = sidebarContainer.innerHTML + button;
+    _addClickListenerToCopyPermalinkButton(roadmapCardId);
+  }
+}
+
+const observer = new MutationObserver(() => {
+  if (document.querySelector('[id^="card-persona-menu-"]')) {
+    setTimeout(addCopyPermalinkButtonToSidebar(), 500);
+  }
+});
+observer.observe(document, { subtree: true, childList: true });
+
+// private
+
+function _addClickListenerToCopyPermalinkButton(roadmapCardId) {
+  document.body.addEventListener("click", function (event) {
+    if (event.target.id == "sobe__copy-permalink-button") {
+      _copyCardPermalinkToClipboard(roadmapCardId);
+    }
+  });
+}
+
+function _copyCardPermalinkToClipboard(roadmapCardId) {
+  if (document.querySelector("#sobe__copy-permalink-button")) {
+    var newLocation = _getPermalinkUrl(roadmapCardId);
+    _updateCopyPermalinkText();
+    navigator.clipboard.writeText(newLocation);
+  }
+}
+
+function _getPermalinkUrl(roadmapCardId) {
+  var splitLocation = window.location
+    .toString()
+    .replace("/r/", "/m/")
+    .split("/");
+  splitLocation[splitLocation.length - 1] = `!card/${roadmapCardId}`;
+  return splitLocation.join("/");
+}
+
+function _updateCopyPermalinkText() {
+  document.querySelector("#sobe__copy-permalink-button").innerHTML = "Copied!";
+  setTimeout(() => {
+    document.querySelector("#sobe__copy-permalink-button").innerHTML =
+      "Copy permalink";
+  }, "1500");
+}
